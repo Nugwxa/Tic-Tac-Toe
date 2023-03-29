5cfb32eb75d0c6fccad75b3b713ea7a4574d07ca
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2023-03-30 00:24:37 +0100

    Added checkWinner()  and refactored checkBoard()

--- mainClasses.h
+++ mainClasses.h
@@ -4,18 +4,15 @@
 #ifndef MAINCLASSES_H
 #define MAINCLASSES_H
 
-// Header file code here
-
-
-
-
 class Game {
     char gameBoard[4][4];
 public:
     Game(); // Initialise Points
     void showBoard();
     void fillBoard(int row, int column, char mark);
-    void CheckBoard();
+    void checkWinner(int xCount, int oCount, bool &xWin, bool &oWin);
+    void checkBoard(bool& xWin, bool& oWin);
+    
 };
 
 
@@ -28,13 +25,5 @@ public:
     void setName(std::string uName);
     void makeMove(Game& gameBoard, int row, int column);
 };
-
-
-
-// [0, 1, 2, 3]
-// [0, 1, 2, 3]
-// [0, 1, 2, 3]
-// [0, 1, 2, 3]
-
 
 #endif
